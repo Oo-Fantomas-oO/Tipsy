ab615a4f5c3fccf5172a056e073bf28e91f7ab3b
Fantomas <1234556789>
2021-02-11 11:31:57 +0200

    Created IBOutlets and IBAction, rename ViewController to CalculateViewController

--- Tipsy/Controllers/CalculateViewController.swift
+++ Tipsy/Controllers/CalculateViewController.swift
@@ -1,19 +1,42 @@
 //
-//  ViewController.swift
+//  CalculateViewController.swift
 //  Tipsy
 //
-//  Created by Onopriienko.Sergii on 10.02.2021.
+//  Created by Onopriienko.Sergii on 11.02.2021.
 //
 
 import UIKit
 
 class CalculateViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
 }
-
